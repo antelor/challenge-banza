17faaab033b2c3eebf1d50ec46f4a212e7efc955
antelor <ramiro.antelo4@gmail.com>
2025-05-28 10:15:28 -0300

    fix navbar css for small displays

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,11 +4,11 @@ import { RandomButton } from "./RandomButton";
 
 export default function Navbar() {
   return (
-    <nav className="w-full px-4 py-3 bg-white border-b shadow-sm flex items-center justify-between">
+    <nav className="w-auto px-4 py-3 bg-white border-b shadow-sm flex items-center justify-between">
       <Link href="/" className="text-lg font-bold">
         Art Institute of Chicago
       </Link>
-      <div className="space-x-2">
+      <div className="flex flex-wrap gap-2">
         <Link href="/favorites">
           <Button variant="outline" size="sm" className="bg-yellow-300">Favorites</Button>
         </Link>
